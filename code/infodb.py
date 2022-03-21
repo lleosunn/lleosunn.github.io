ae3b00aa9a2bb14b312de2b4a7d75388ebb6c066
lleosunn <89223570+lleosunn@users.noreply.github.com>
2022-03-21 07:44:20 +0000

    infodb

--- code/infodb.py
+++ code/infodb.py
@@ -1,34 +1,34 @@
 InfoDb = []
 # List with dictionary records placed in a list  
 InfoDb.append({  
-	"FirstName": "LeBron",  
-    "LastName": "James",  
-	"Played_For":["Cavs","Lakers","Heat"]  
+	"FirstName": "Leo",  
+  "LastName": "Sun",  
+	"Favorite Colors":["Red","Orange","Yellow"]  
 })  
 
 InfoDb.append({  
-	"FirstName": "Stephen",  
-	"LastName": "Curry",  
-	"Owns_Cars":["Warriors"]  
+	"FirstName": "Stanley",  
+	"LastName": "Yan",  
+	"Favorite Colors":["Blue", "Green", "Purple"]  
 })  
 
 InfoDb.append({  
-	"FirstName": "Dwyane",  
-	"LastName": "Wade",  
-	"Played_For":["Heat", "Cavs", "Bulls"]  
+	"FirstName": "Ur",  
+	"LastName": "Mom",  
+	"Favorite Colors":["Black", "Grey", "White"]  
 })
 
-def for_loop():
+def for_loop(): #iterates on length of the list
 	for i in range(len(InfoDb)):
 		print(InfoDb[i])
 
-def while_loop():
+def while_loop(): #initial index n and increments by 1
 	i = 0
 	while i < len(InfoDb):
 		print(InfoDb[i])
 		i = i+1
 
-def recursive_loop(i = 0):
+def recursive_loop(i = 0): #loop increments on n + 1 until each condition is met
 	if i < len(InfoDb):
 		print(InfoDb[i])
 		recursive_loop(i + 1)
