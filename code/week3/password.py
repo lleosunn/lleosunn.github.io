c2f10b01a73115b7022c10002330e63e2f046b4f
Aadit Gupta <50186752+aaditgupta21@users.noreply.github.com>
2022-03-29 22:12:21 +0000

    Finish Password Generator

--- code/week3/password.py
+++ code/week3/password.py
@@ -4,3 +4,15 @@ import string
 all = list(string.ascii_letters + string.digits + "!@#$%^&*()")
 
 
+def gen():
+	passw = int(input("Enter password length: "))
+	random.shuffle(all)
+	password = []
+	for i in range(passw):
+		password.append(random.choice(all))
+	
+	random.shuffle(password)
+	
+	print("".join(password))
+
+gen()
